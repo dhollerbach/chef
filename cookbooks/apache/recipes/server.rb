3cba88e4c787a73a5b29943b402f23565627c279
Dave Hollerbach <dave.hollerbach@gmail.com>
2018-12-29 00:10:06 +0000

    praciticing a few additional commands

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -4,14 +4,34 @@
 #
 # Copyright (c) 2018 The Authors, All Rights Reserved.
 
-package ['httpd'] do
-  action :install
+# notifies :action, 'resource[name]', :timer
+# subscribes :action, 'resource[name]', :timer
+#
+# :before, :delayed, :immediately
+
+package 'httpd'
+
+=begin
+cookbook_file '/var/www/html/index.html' do
+  source 'index.html'
+end
+=end
+
+remote_file '/var/www/html/derp_kitty.jpg' do
+  source 'https://i.imgur.com/JWxxL.jpg'
 end
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
-  action :create
+  notifies :restart, 'service[httpd]', :immediately
 end
+
+=begin
+directory '/var/www/mysites' do
+  owner 'apache'
+  recursive true
+end
+=end
 
 service 'httpd' do
   action [ :enable, :start ]
--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -1,11 +1,22 @@
-package ['tree', 'git', 'ntp'] do
-  action :install
-end
+package ['tree', 'git', 'ntp']
 
 template '/etc/motd' do
-  action :create
   source 'motd.erb'
   variables(
     :name => 'Dave Hollerbach'
   )
+  owner 'root'
+  group 'root'
+end
+
+user 'user1' do
+  comment 'user1'
+  uid '123'
+  home '/home/user1/'
+  shell '/bin/bash'
+end
+
+group 'admins' do
+  members 'user1'
+  append true
 end
